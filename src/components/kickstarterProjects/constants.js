1381da62c128b67c1ae461ae3495482652dd8cb7
booleanShudhanshu <shudhanshuraj17@gmail.com>
2024-12-14 14:11:35 +0530

    css update

--- src/components/kickstarterProjects/constants.js
+++ src/components/kickstarterProjects/constants.js
@@ -1,8 +1,8 @@
 // coulmns to display in table
 export const COLUMNS = [
     {display_name: "S.No.", key: "s.no",  }, 
-    {display_name: "Percentage funded", key: "percentage.funded" , formatter: (val, maxlen) => `${val}`.padStart(maxlen, " "),  }, 
-    {display_name: "Amount pledged", key: "amt.pledged", formatter: (val, maxlen) => `${val}`.padStart(maxlen, " ") }
+    {display_name: "Percentage funded", key: "percentage.funded" , formatter: (val, maxlen) => `${val}`.padStart(maxlen, "0"),  }, 
+    {display_name: "Amount pledged", key: "amt.pledged", formatter: (val, maxlen) => `${val}`.padStart(maxlen, "0") }
 ]
 
 // all coulmns
